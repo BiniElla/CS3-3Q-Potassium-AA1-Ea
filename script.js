24d126c9dd98535b00ba9fd7e406b64f78b13037
BiniElla <mrdea22@src.pshs.edu.ph>
2025-01-22 15:50:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let c = 2 * pi * r;
 let s = 4 * pi * (r ** 2);
 let v = 4 / 3 * pi * (r ** 3);
 
-document.write('The radius of the sphere is ' + r );
+document.write('The radius of the sphere is ', + r );
 document.write("<br></br>");
-document.write('The circumference is ' + c);
+document.write('The circumference is ', + c);
 document.write("<br></br>");
-document.write('The surface area is ' + s);
+document.write('The surface area is ', + s);
 document.write("<br></br>");
-document.write('The volume is ' + v);
+document.write('The volume is ', + v);
