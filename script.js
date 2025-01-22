547e0ec59c06972e068d6ebf6dd23947d4d745b2
BiniElla <mrdea22@src.pshs.edu.ph>
2025-01-22 11:01:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let pi = 3.14;
+let r;
+let c = 2 * pi * r;
+let s = 4 * pi * r ** 2;
+let v = 4 / 3 * pi * r ** 3;
+
+document.write('The radius of the sphere is', r );
+document.write('The circumference is', c);
+document.write('The surface area is', s);
+document.write('The volume is', v);
